d4718a64756267c36e15e34c16001626eafc2109
smoonmare <smoonmare@gmail.com>
2020-12-02 14:09:55 -0800

    Contour Box. Added function to display contour around list of colors

--- open_cv/project-1/virtual_marker.py
+++ open_cv/project-1/virtual_marker.py
@@ -6,17 +6,34 @@ frame_width = 640
 frame_height = 480
 # colors red, green, blue, yellow found with color_picker.py
 # color = [hue_min, sat_min, val_min, hue_max, sat_max, val_max]
-colors = [[164, 195, 45, 179, 255, 255],
+colors = [[148, 195, 60, 179, 255, 255],
           [31, 71, 0, 76, 255, 255],
-          [86, 106, 77, 172, 255, 255],
+          [89, 138, 51, 102, 255, 255],
           [17, 60, 39, 26, 255, 255]]
+
+
+def getContour(img):
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    for contour in contours:
+        area = cv2.contourArea(contour)
+
+        if area > 500:
+            perimeter = cv2.arcLength(contour, True)
+            approx = cv2.approxPolyDP(contour, 0.02 * perimeter, True)
+            x, y, w, h = cv2.boundingRect(approx)
+            cv2.drawContours(imgResult, contour, -1, (255, 0, 0), 2)
+
 
 def findColor(img, *args):
     imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    lower_hsv = np.array(colors[0][0:3])
-    upper_hsv = np.array(colors[0][3:6])
-    mask = cv2.inRange(imgHSV, lower_hsv, upper_hsv)
-    cv2.imshow("Img", mask)
+
+    for color in colors:
+        lower_hsv = np.array(color[0:3])
+        upper_hsv = np.array(color[3:6])
+        mask = cv2.inRange(imgHSV, lower_hsv, upper_hsv)
+        getContour(mask)
+        # cv2.imshow(str(color[0]), mask)
 
 
 capture = cv2.VideoCapture(0)
@@ -48,8 +65,10 @@ capture.set(10, 25)
 
 while True:
     success, img = capture.read()
+    imgResult = img.copy()
 
     findColor(img, colors)
     cv2.imshow("Live Feed Output", img)
+    cv2.imshow("Contour Box", imgResult)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
