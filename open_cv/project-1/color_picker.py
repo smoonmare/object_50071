def097efe57075fb915ab1865d3a1a688443c74d
smoonmare <smoonmare@gmail.com>
2020-12-02 13:42:23 -0800

    Color picker script. Using HSV Trackbar to get attr of decired color. Main project can recognize 5 sharpies.

---
+++ open_cv/project-1/color_picker.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+
+def empty(a):
+    pass
+
+
+frame_width = 640
+frame_height = 480
+capture = cv2.VideoCapture(0)
+capture.set(3, frame_width)
+capture.set(4, frame_height)
+capture.set(11, 45)
+capture.set(10, 25)
+
+cv2.namedWindow("HSV")
+cv2.resizeWindow("HSV", 640, 240)
+cv2.createTrackbar("Hue min", "HSV", 0, 179, empty)
+cv2.createTrackbar("Sat min", "HSV", 0, 255, empty)
+cv2.createTrackbar("Val min", "HSV", 0, 255, empty)
+cv2.createTrackbar("Hue max", "HSV", 179, 179 , empty)
+cv2.createTrackbar("Sat max", "HSV", 255, 255, empty)
+cv2.createTrackbar("Val max", "HSV", 255, 255, empty)
+
+while True:
+    _, img = capture.read()
+    imgHsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    h_min = cv2.getTrackbarPos("Hue min", "HSV")
+    h_max = cv2.getTrackbarPos("Hue max", "HSV")
+    s_min = cv2.getTrackbarPos("Sat min", "HSV")
+    s_max = cv2.getTrackbarPos("Sat max", "HSV")
+    v_min = cv2.getTrackbarPos("Val min", "HSV")
+    v_max = cv2.getTrackbarPos("Val max", "HSV")
+    print(h_min)
+
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(imgHsv, lower, upper)
+    result = cv2.bitwise_and(img, img, mask=mask)
+    mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+    h_stack = np.hstack([img, mask, result])
+    # cv2.imshow('Original', img)
+    # cv2.imshow('HSV Color Space', imgHsv)
+    # cv2.imshow('Mask', mask)
+    # cv2.imshow('Result', result)
+    cv2.imshow('Horizontal Stack', h_stack)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
---
+++ open_cv/project-1/virtual_marker.py
@@ -0,0 +1,55 @@
+import cv2
+import numpy as np
+
+# Variables
+frame_width = 640
+frame_height = 480
+# colors red, green, blue, yellow found with color_picker.py
+# color = [hue_min, sat_min, val_min, hue_max, sat_max, val_max]
+colors = [[164, 195, 45, 179, 255, 255],
+          [31, 71, 0, 76, 255, 255],
+          [86, 106, 77, 172, 255, 255],
+          [17, 60, 39, 26, 255, 255]]
+
+def findColor(img, *args):
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    lower_hsv = np.array(colors[0][0:3])
+    upper_hsv = np.array(colors[0][3:6])
+    mask = cv2.inRange(imgHSV, lower_hsv, upper_hsv)
+    cv2.imshow("Img", mask)
+
+
+capture = cv2.VideoCapture(0)
+capture.set(3, frame_width)
+capture.set(4, frame_height)
+capture.set(11, 45)
+capture.set(10, 25)
+# .set() properties
+# 0. CV_CAP_PROP_POS_MSEC Current position of the video file in milliseconds.
+# 1. CV_CAP_PROP_POS_FRAMES 0-based index of the frame to be decoded/captured next.
+# 2. CV_CAP_PROP_POS_AVI_RATIO Relative position of the video file
+# 3. CV_CAP_PROP_FRAME_WIDTH Width of the frames in the video stream.
+# 4. CV_CAP_PROP_FRAME_HEIGHT Height of the frames in the video stream.
+# 5. CV_CAP_PROP_FPS Frame rate.
+# 6. CV_CAP_PROP_FOURCC 4-character code of codec.
+# 7. CV_CAP_PROP_FRAME_COUNT Number of frames in the video file.
+# 8. CV_CAP_PROP_FORMAT Format of the Mat objects returned by retrieve() .
+# 9. CV_CAP_PROP_MODE Backend-specific value indicating the current capture mode.
+# 10. CV_CAP_PROP_BRIGHTNESS Brightness of the image (only for cameras).
+# 11. CV_CAP_PROP_CONTRAST Contrast of the image (only for cameras).
+# 12. CV_CAP_PROP_SATURATION Saturation of the image (only for cameras).
+# 13. CV_CAP_PROP_HUE Hue of the image (only for cameras).
+# 14. CV_CAP_PROP_GAIN Gain of the image (only for cameras).
+# 15. CV_CAP_PROP_EXPOSURE Exposure (only for cameras).
+# 16. CV_CAP_PROP_CONVERT_RGB Boolean flags indicating whether images should be converted to RGB.
+# 17. CV_CAP_PROP_WHITE_BALANCE Currently unsupported
+# 18. CV_CAP_PROP_RECTIFICATION Rectification flag for stereo cameras
+#     (note: only supported by DC1394 v 2.x backend currently)
+
+while True:
+    success, img = capture.read()
+
+    findColor(img, colors)
+    cv2.imshow("Live Feed Output", img)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
