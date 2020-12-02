6582a0f6ad4164cc0a70718320d2c1d5723c94af
smoonmare <smoonmare@gmail.com>
2020-12-01 19:42:43 -0800

    Changing imported image color to gray using cvtColor()

---
+++ open_cv/chapter_2_1.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python3.7
+import cv2
+
+img = cv2.imread("test_pic.jpg")
+
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+cv2.imshow("Gray Image", imgGray)
+cv2.waitKey(0)
---
+++ open_cv/chapter_2_2.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3.7
+import cv2
+
+img = cv2.imread("test_pic.jpg")
+
+imgBlur = cv2.GaussianBlur(img, (7, 7), 0)
+
+cv2.imshow("Regular Image", img)
+cv2.imshow("Blurred Image", imgBlur)
+cv2.waitKey(0)
