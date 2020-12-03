8b4abd52181e9e5ea6ee689f10a46c46c2e61c74
smoonmare <smoonmare@gmail.com>
2020-12-03 14:36:54 -0800

    Added image stacking. Warp now crops input image. Added comments

--- open_cv/project-2/scanner.py
+++ open_cv/project-2/scanner.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 
+
+feed_list = []
+doc_view = []
 kernel = np.ones((5, 5))
 img_width = 640
 img_height = 480
@@ -33,13 +36,13 @@ def get_contour(img_in):
     contours, hierarchy = cv2.findContours(img_in, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     for contour in contours:
         area = cv2.contourArea(contour)
-        print(area)
+        # print(area)
         if area > 60000:
             perimeter = cv2.arcLength(contour, True)
-            print(perimeter)
+            # print(perimeter)
             approx = cv2.approxPolyDP(contour, 0.02 * perimeter, True)
-            print(approx)
-            print(len(approx))
+            # print(approx)
+            # print(len(approx))
             # Corner points of the contour  figures
             if area > max_area and len(approx) == 4:
                 corner_points = approx
@@ -78,14 +81,64 @@ def get_warp(img_in, points):
     :param points: corner points of the document
     :return: warped image
     """
-    print(points)
+    # print(points)
     points = reorder_points(points)
     point_1 = np.array(points, np.float32)
     point_2 = np.array([[0, 0], [img_width, 0], [0, img_height], [img_width, img_height]], np.float32)
     matrix = cv2.getPerspectiveTransform(point_1, point_2)
     img_out = cv2.warpPerspective(img_in, matrix, (img_width, img_height))
-
+    img_out = img_out[20:img_out.shape[0]-20, 20:img_out.shape[1]-20]
+    img_out = cv2.resize(img_out, (img_width, img_height))
     return img_out
+
+
+def stack_images(scale, img_array):
+    """
+    Function for stacking images.
+    :param scale: scaling multiplier
+    :param img_array: an array of images for stack
+    img_array = (row[item1, item2],
+                 row[item3, item4],
+                 row[item5, item6])
+    :return: stack of images
+    Source: https://github.com/murtazahassan/Learn-OpenCV-in-3-hours/blob/master/chapter6.py
+    """
+    rows = len(img_array)
+    columns = len(img_array[0])
+    rows_available = isinstance(img_array[0], list)
+    width = img_array[0][0].shape[1]
+    height = img_array[0][0].shape[0]
+    if rows_available:
+        for x in range(0, rows):
+            for y in range(0, columns):
+                if img_array[x][y].shape[:2] == img_array[0][0].shape[:2]:
+                    img_array[x][y] = cv2.resize(img_array[x][y],
+                                                 (0, 0),
+                                                 None, scale, scale)
+                else:
+                    img_array[x][y] = cv2.resize(img_array[x][y],
+                                                 (img_array[0][0].shape[1], img_array[0][0].shape[0]),
+                                                 None, scale, scale)
+                if len(img_array[x][y].shape) == 2:
+                    img_array[x][y] = cv2.cvtColor(img_array[x][y], cv2.COLOR_GRAY2BGR)
+        img_blank = np.zeros((height, width, 3), np.uint8)
+        horizontal = [img_blank] * rows
+        for x in range(0, rows):
+            horizontal[x] = np.hstack(img_array[x])
+        vertical = np.vstack(horizontal)
+    else:
+        for x in range(0, rows):
+            if img_array[x].shape[:2] == img_array[0].shape[:2]:
+                img_array[x] = cv2.resize(img_array[x], (0, 0), None, scale, scale)
+            else:
+                img_array[x] = cv2.resize(img_array[x],
+                                          (img_array[0].shape[1], img_array[0].shape[0]),
+                                          None, scale, scale)
+            if len(img_array[x].shape) == 2:
+                img_array[x] = cv2.cvtColor(img_array[x], cv2.COLOR_GRAY2BGR)
+        horizontal = np.hstack(img_array)
+        vertical = horizontal
+    return vertical
 
 
 while True:
@@ -93,17 +146,19 @@ while True:
     img = cv2.resize(img, (img_width, img_height))
     img_contour = img.copy()
     img_thresh = pre_processing(img, kernel)
+    contour_points = get_contour(img_thresh)
     try:
-        contour_points = get_contour(img_thresh)
         img_warp = get_warp(img, contour_points)
-        cv2.imshow("Contour Box", img_contour)
-        cv2.imshow("Warped Image", img_warp)
+        doc_view = ([img_warp], [img_contour])
+        stack_doc_view = stack_images(0.7, doc_view)
+        cv2.imshow("Found Document", stack_doc_view)
     except ValueError:
         print("No document detected")
     except AttributeError:
-        print("Moving too fast")
-    cv2.imshow("Live Feed Output", img)
-    cv2.imshow("Image Threshold", img_thresh)
+        print("Reshaping is broken if no lines detected")
+    feed_list = ([img], [img_thresh])
+    stack_feed_list = stack_images(0.7, feed_list)
+    cv2.imshow("Feed / Threshold", stack_feed_list)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         cv2.destroyAllWindows()
         break
