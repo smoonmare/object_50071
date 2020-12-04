2f29a12816ad7c8b84baa755b21ef33548aa086a
smoonmare <smoonmare@gmail.com>
2020-12-03 16:58:03 -0800

    Styled code

--- open_cv/project-1/virtual_marker.py
+++ open_cv/project-1/virtual_marker.py
@@ -18,42 +18,108 @@ points = []
 # [x, y, color_id]
 
 
-def getContour(img):
-    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+def get_contour(img_in):
+    """
+    Function to get center contour point
+    :param img_in: input image
+    :return: center of contour point
+    """
     x, y, w, h = 0, 0, 0, 0
-
+    contours, hierarchy = cv2.findContours(img_in, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     for contour in contours:
         area = cv2.contourArea(contour)
-
         if area > 500:
             perimeter = cv2.arcLength(contour, True)
             approx = cv2.approxPolyDP(contour, 0.02 * perimeter, True)
             x, y, w, h = cv2.boundingRect(approx)
-            # cv2.drawContours(imgResult, contour, -1, (255, 0, 0), 2)
+            # cv2.draw_contours(img_result, contour, -1, (255, 0, 0), 2)
     return x + w // 2, y
 
 
-def findColor(img, *args):
-    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+def find_color(img_in, hsv, brg):
+    """
+    Function to detect specific color
+    :param img_in: input image
+    :param hsv: HSV color array
+    :param brg: BRG color array
+    :return: an array of points
+    """
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     color_number = 0
-    new_points = []
+    points_out = []
 
-    for color_hsv in colors_hsv:
-        lower_hsv = np.array(color_hsv[0:3])
-        upper_hsv = np.array(color_hsv[3:6])
-        mask = cv2.inRange(imgHSV, lower_hsv, upper_hsv)
-        x, y = getContour(mask)
-        cv2.circle(imgResult, (x, y), 20, colors_bgr[color_number], cv2.FILLED)
+    for hsv in colors_hsv:
+        lower_hsv = np.array(hsv[0:3])
+        upper_hsv = np.array(hsv[3:6])
+        mask = cv2.inRange(img_hsv, lower_hsv, upper_hsv)
+        x, y = get_contour(mask)
+        cv2.circle(img_in, (x, y), 20, brg[color_number], cv2.FILLED)
         if x != 0 and y != 0:
-            points.append([x, y, color_number])
+            points_out.append([x, y, color_number])
         color_number += 1
         # cv2.imshow(str(color[0]), mask)
-    return new_points
+    return points_out
 
 
-def drawOnCanvas(points, color_brg):
-    for point in points:
-        cv2.circle(imgResult, (point[0], point[1]), 20, colors_bgr[point[2]], cv2.FILLED)
+def draw_on_canvas(canvas, points_in, colors_brg_in):
+    """
+    Drawing on input canvas with detected points
+    :param canvas: input canvas
+    :param points_in: on sharpie
+    :param colors_brg_in: colors of  sharpies
+    :return: nothing
+    """
+    for point in points_in:
+        cv2.circle(canvas, (point[0], point[1]), 20, colors_brg_in[point[2]], cv2.FILLED)
+
+
+def stack_images(scale, img_array):
+    """
+    Function for stacking images.
+    :param scale: scaling multiplier
+    :param img_array: an array of images for stack
+    img_array = (row[item1, item2],
+                 row[item3, item4],
+                 row[item5, item6])
+    :return: stack of images
+    Source: https://github.com/murtazahassan/Learn-OpenCV-in-3-hours/blob/master/chapter6.py
+    """
+    rows = len(img_array)
+    columns = len(img_array[0])
+    rows_available = isinstance(img_array[0], list)
+    width = img_array[0][0].shape[1]
+    height = img_array[0][0].shape[0]
+    if rows_available:
+        for x in range(0, rows):
+            for y in range(0, columns):
+                if img_array[x][y].shape[:2] == img_array[0][0].shape[:2]:
+                    img_array[x][y] = cv2.resize(img_array[x][y],
+                                                 (0, 0),
+                                                 None, scale, scale)
+                else:
+                    img_array[x][y] = cv2.resize(img_array[x][y],
+                                                 (img_array[0][0].shape[1], img_array[0][0].shape[0]),
+                                                 None, scale, scale)
+                if len(img_array[x][y].shape) == 2:
+                    img_array[x][y] = cv2.cvtColor(img_array[x][y], cv2.COLOR_GRAY2BGR)
+        img_blank = np.zeros((height, width, 3), np.uint8)
+        horizontal = [img_blank] * rows
+        for x in range(0, rows):
+            horizontal[x] = np.hstack(img_array[x])
+        vertical = np.vstack(horizontal)
+    else:
+        for x in range(0, rows):
+            if img_array[x].shape[:2] == img_array[0].shape[:2]:
+                img_array[x] = cv2.resize(img_array[x], (0, 0), None, scale, scale)
+            else:
+                img_array[x] = cv2.resize(img_array[x],
+                                          (img_array[0].shape[1], img_array[0].shape[0]),
+                                          None, scale, scale)
+            if len(img_array[x].shape) == 2:
+                img_array[x] = cv2.cvtColor(img_array[x], cv2.COLOR_GRAY2BGR)
+        horizontal = np.hstack(img_array)
+        vertical = horizontal
+    return vertical
 
 
 capture = cv2.VideoCapture(0)
@@ -61,40 +127,23 @@ capture.set(3, frame_width)
 capture.set(4, frame_height)
 capture.set(11, 45)
 capture.set(10, 25)
-# .set() properties
-# 0. CV_CAP_PROP_POS_MSEC Current position of the video file in milliseconds.
-# 1. CV_CAP_PROP_POS_FRAMES 0-based index of the frame to be decoded/captured next.
-# 2. CV_CAP_PROP_POS_AVI_RATIO Relative position of the video file
-# 3. CV_CAP_PROP_FRAME_WIDTH Width of the frames in the video stream.
-# 4. CV_CAP_PROP_FRAME_HEIGHT Height of the frames in the video stream.
-# 5. CV_CAP_PROP_FPS Frame rate.
-# 6. CV_CAP_PROP_FOURCC 4-character code of codec.
-# 7. CV_CAP_PROP_FRAME_COUNT Number of frames in the video file.
-# 8. CV_CAP_PROP_FORMAT Format of the Mat objects returned by retrieve() .
-# 9. CV_CAP_PROP_MODE Backend-specific value indicating the current capture mode.
-# 10. CV_CAP_PROP_BRIGHTNESS Brightness of the image (only for cameras).
-# 11. CV_CAP_PROP_CONTRAST Contrast of the image (only for cameras).
-# 12. CV_CAP_PROP_SATURATION Saturation of the image (only for cameras).
-# 13. CV_CAP_PROP_HUE Hue of the image (only for cameras).
-# 14. CV_CAP_PROP_GAIN Gain of the image (only for cameras).
-# 15. CV_CAP_PROP_EXPOSURE Exposure (only for cameras).
-# 16. CV_CAP_PROP_CONVERT_RGB Boolean flags indicating whether images should be converted to RGB.
-# 17. CV_CAP_PROP_WHITE_BALANCE Currently unsupported
-# 18. CV_CAP_PROP_RECTIFICATION Rectification flag for stereo cameras
-#     (note: only supported by DC1394 v 2.x backend currently)
 
 while True:
     success, img = capture.read()
-    imgResult = img.copy()
-
-    new_points = findColor(img, colors_hsv, colors_bgr)
-    if len(new_points) != 0:
-        for new_point in new_points:
-            points.append(new_point)
-    if len(points) != 0:
-        drawOnCanvas(points, colors_bgr)
-
-    cv2.imshow("Live Feed Output", img)
-    cv2.imshow("Contour Box", imgResult)
-    if cv2.waitKey(1) & 0xFF == ord("q"):
+    img_canvas = img.copy()
+    key = cv2.waitKey(1)
+    if key == ord("q"):
+        cv2.destroyAllWindows()
         break
+    else:
+        new_points = find_color(img, colors_hsv, colors_bgr)
+        if len(new_points) != 0:
+            for new_point in new_points:
+                points.append(new_point)
+        if len(points) != 0:
+            draw_on_canvas(img_canvas, points, colors_bgr)
+        stack_feed_list = ([img_canvas, img])
+        stack_feed = stack_images(0.9, stack_feed_list)
+        cv2.imshow("Live Feed / Drawing", stack_feed)
+        # cv2.imshow("Live Feed Output", img)
+        # cv2.imshow("Contour Box", img_result)
