cbf4c8c1f6d1ecb2390a4c0edc5b224df72dbcb5
smoonmare <smoonmare@gmail.com>
2021-01-07 14:47:06 -0800

    Updated to add regex explanation.

--- python-problems/hackerrank/python/matrix-script/run.py
+++ python-problems/hackerrank/python/matrix-script/run.py
@@ -20,3 +20,22 @@ for column in range(m):
         message += matrix[row][column]
 decoded_message = re.sub(r'\b[^a-zA-Z0-9]+\b', ' ', message)
 print(decoded_message)
+'''
+Test string:
+    This$#is% Matrix#  %!
+Explanation:
+\b[^a-zA-Z0-9]+\b / gm
+--- \b assert position at a word boundary: (^\\w|\\w$|\\W\\w|\\w\\W)
+    Match a single character not present in the list below [^a-zA-Z0-9]+
+    -  + Quantifier — Matches between one and unlimited times, as many times as possible, giving back as needed (greedy)
+    -  a-z a single character in the range between a (index 97) and z (index 122) (case sensitive)
+    -  A-Z a single character in the range between A (index 65) and Z (index 90) (case sensitive)
+    -  0-9 a single character in the range between 0 (index 48) and 9 (index 57) (case sensitive)
+--- \b assert position at a word boundary: (^\\w|\\w$|\\W\\w|\\w\\W)
+    Global pattern flags
+    -  g modifier: global. All matches (don't return after first match)
+    -  m modifier: multi line. Causes ^ and $ to match the begin/end of each line (not only begin/end of string)
+Match information:
+    Match 1: Full match 4-6: $#
+    Match 2: Full match 8-10: %[space]
+'''
