74910e7d8a99662bc578f48353e4507c2c55af2a
smoonmare <smoonmare@gmail.com>
2021-01-15 17:22:45 -0800

    Day 12 solution

---
+++ python-problems/hackerrank/student-score-avg/run.py
@@ -0,0 +1,42 @@
+class Person:
+	def __init__(self, firstName, lastName, idNumber):
+		self.firstName = firstName
+		self.lastName = lastName
+		self.idNumber = idNumber
+
+	def printPerson(self):
+		print("Name:", self.lastName + ",", self.firstName)
+		print("ID:", self.idNumber)
+
+class Student(Person):
+    def __init__(self, firstName, lastName, idNumber, testScores):
+        super().__init__(firstName, lastName, idNumber)
+        self.testScores = testScores
+
+    def calculate(self):
+        total = 0
+        for score in self.testScores:
+            total += score
+        avg = total / len(self.testScores)
+        if avg >= 90:
+            return 'O'
+        elif 80 <= avg < 90:
+            return 'E'
+        elif 70 <= avg < 80:
+            return 'A'
+        elif 55 <= avg < 70:
+            return 'P'
+        elif 40 <= avg < 55:
+            return 'D'
+        else:
+            return 'T'
+        
+line = input().split()
+firstName = line[0]
+lastName = line[1]
+idNum = line[2]
+numScores = int(input())
+scores = list( map(int, input().split()) )
+s = Student(firstName, lastName, idNum, scores)
+s.printPerson()
+print("Grade:", s.calculate())
